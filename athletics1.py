6013e024ce920a9894c79504482b7f1035167b86
Tomaz Chevres <tchevres-24@peddie.org>
2022-11-04 00:03:47 -0400

    Changed athletics2.py to read updated site

--- athletics1.py
+++ athletics1.py
@@ -13,7 +13,8 @@ data = requests.get('https://www.peddie.org/athletics/traditions/blair-rivalry')
 #load data into bs4
 soup = BeautifulSoup(data.text, 'html.parser')
 
-table = soup.find('div', {'id':'fsEl_36188'}).find('tbody').find_all('tr')
+table = soup.find('section', {'id':'fsEl_37420'}).find('tbody').find_all('tr')
+#table = soup.find('section', {'id':'fsEl_37421'}).find('tbody').find_all('tr') #testing
 
 for row in table:
     if row.find('span', {'class':'fsAthleticsStatusCancelled'}) is None:
@@ -22,8 +23,12 @@ for row in table:
         item["location"] = row.find('td', {'class':'fsAthleticsLocations'}).text.replace('\n',"").replace('\t',"")
         item["time"] = row.find('time', {'class':'fsTime'}).get("datetime").split("T")[1]
         item["time"] = str(int(item["time"].split(":")[0])%12) +":"+ item["time"].split(":")[1] #read and set correct time from datetime attribute
+        result = row.find('td', {'class':'fsAthleticsResult'}).text.replace('\n',"").replace('\t',"")
+        result = ("Peddie" if (result=="Win") else "Blair" if (result=="Lose") else "")
+        score = row.find('td', {'class':'fsAthleticsScore'}).text.replace('\n',"").replace('\t',"")
+        item["score"] = result + " " + score
 
-        file1.write(item["team"] +'\\'+ item["location"] +'\\'+ item["time"] + "\\\n")
+        file1.write(item["team"] +'\\'+ item["location"] +'\\'+ item["time"] + "\\" + item["score"] + "\n")
 # file2.write(menu2)
 file1.close()
 # file2.close()
--- athletics2.py
+++ athletics2.py
@@ -5,21 +5,45 @@ events=Path('athletics1.txt').read_text().split("\n")
 
 override=Path('athleticsOverride.txt').read_text().split("\n")
 
-for i in range(0,len(events)-1):
-    event = events[i].split("\\")
+# for i in range(0,len(events)-1):
+#     event = events[i].split("\\")
 
-    for item in override:
-        if item.split("\\")[0] == event[0]:
-            for j in range(1,len(item.split("\\"))):
-                if len(item.split("\\")[j]) > 0:
-                    event[j-1] = item.split("\\")[j] 
+#     for item in override:
+#         if item.split("\\")[0] == event[0]:
+#             for j in range(1,len(item.split("\\"))):
+#                 if len(item.split("\\")[j]) > 0:
+#                     event[j-1] = item.split("\\")[j] 
     
+#     for k in range(len(event)):
+#         file1.write(event[k])
+#         if k+1 < len(event):
+#             file1.write("\\")
+#     file1.write("\n")
+
+for i in range(0,len(override)):
+    oEvent = override[i].split("\\")
+
+    if(len(oEvent)==5):
+        if(oEvent[0]=="+"):
+            events.insert(0,override[i][2:])
+        else:
+            for j in range(0,len(events)):
+                event = events[j].split("\\")
+                if(event[0]==oEvent[0]):
+                    for k in range(1,len(oEvent)):
+                        if(len(oEvent[k])>0):
+                            event[k-1] = oEvent[k]
+                    events[j]=event[0]
+                    for k in range(1,len(oEvent)-1):
+                        events[j]+="\\"+event[k]
+
+for i in range(len(events)-1):
+    event = events[i].split("\\")
     for k in range(len(event)):
         file1.write(event[k])
         if k+1 < len(event):
             file1.write("\\")
     file1.write("\n")
-
 
 file1.close()
 
